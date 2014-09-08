26b5fcc36c166324e7d39001a27e62788b8862aa
Arnout Kazemier <info@3rd-Eden.com>
2014-09-08 11:44:44 +0200

    [fix] Fix npm stats glitch, the npm api is returning incorrect data.

--- endpoints/downloads.js
+++ endpoints/downloads.js
@@ -30,7 +30,20 @@ Downloads.prototype.totals = function point(period, pkg, fn) {
 
   return this.send(['downloads', 'point', period, pkg], {
     api: this.api.statservice
-  }, fn);
+  }, fn).map(function map(data) {
+    //
+    // There is this annoying edge-case in the npm downloads API where they do
+    // not return the correct information for newly published packages. These
+    // newly published packages are missing the `package` and `downloads` field.
+    // We've normalized to their sensible defaults so we can just ignore the
+    // fact that they are unable to correctly estimate the download count for
+    // a new module.
+    //
+    data.package = data.package || pkg;
+    data.downloads = data.downloads || 0;
+
+    return data;
+  });
 };
 
 /**
